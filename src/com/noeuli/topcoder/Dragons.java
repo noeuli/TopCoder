c16f09887de7a556b71aefee5f611b6784db4671
noeuli <noeuli@gmail.com>
2014-07-04 17:16:29 +0900

    SRM 147 Div 1 Level 2 Dragon Completed.

--- src/com/noeuli/topcoder/Dragons.java
+++ src/com/noeuli/topcoder/Dragons.java
@@ -1,109 +1,146 @@
 package com.noeuli.topcoder;
 
+import java.math.BigInteger;
+
 // SRM 147 Div 1 Level 2
 public class Dragons {
-	
-	private final int F = 0;	// Front
-	private final int B = 1;	// Back
-	private final int U = 2;	// Up -the Snaug
-	private final int D = 3;	// Down
-	private final int L = 4;	// Left
-	private final int R = 5;	// Right
+	private static final int F = 0;	// Front
+	private static final int B = 1;	// Back
+	private static final int U = 2;	// Up -the Snaug
+	private static final int D = 3;	// Down
+	private static final int L = 4;	// Left
+	private static final int R = 5;	// Right
+	private static final int SNAUG = U;
+
+	class Fraction {
+	    private BigInteger numerator; // above a line
+	    private BigInteger denominator;   // below a line
+	    
+	    public Fraction() {
+	        numerator = BigInteger.ZERO;
+	        denominator = BigInteger.ONE;
+	    }
+	    
+	    public Fraction (BigInteger value) {
+	        numerator = value;
+	        denominator = BigInteger.ONE;
+	    }
+	    
+	    public Fraction (BigInteger num, BigInteger den) {
+	        numerator = num;
+	        denominator = den;
+	    }
+	    
+	    public Fraction (Fraction a) {
+	        numerator = a.getNumerator();
+	        denominator = a.getDenominator();
+	    }
+	    
+	    public Fraction add(Fraction a) {
+	        numerator = numerator.multiply(a.getDenominator()).add(denominator.multiply(a.getNumerator()));
+	        denominator = denominator.multiply(a.getDenominator());
+	        return reduce();
+	    }
+	    
+	    public Fraction multiply(Fraction a) {
+	        this.numerator = numerator.multiply(a.getNumerator());
+	        this.denominator = denominator.multiply(a.getDenominator());
+	        return reduce();
+	    }
+	    
+	    public void setValue(BigInteger value) {
+	        numerator = value;
+	        denominator = BigInteger.ONE;
+	    }
+	    
+	    public BigInteger getNumerator() {
+	        return numerator;
+	    }
+	    
+	    public BigInteger getDenominator() {
+	        return denominator;
+	    }
+	    
+	    public Fraction reduce() {
+	        BigInteger gcd = numerator.gcd(denominator);
+	        numerator = numerator.divide(gcd);
+	        denominator = denominator.divide(gcd);
+	        return this;
+	    }
+	    
+	    public String toString() {
+	        reduce();
+	        if (BigInteger.ZERO.equals(numerator)) {
+	            return "0";
+	        } else if (BigInteger.ONE.equals(denominator)) {
+	            return String.valueOf(numerator);
+	        } else if (BigInteger.ZERO.equals(denominator)) {
+	            return "Error. Divide by 0.";
+	        } else {
+	            return numerator + "/" + denominator;
+	        }
+	    }
+	}
 	
 	public String snaug(int[] initialFood, int rounds) {
 		String result = "";
 		
-		int[] nextFood = new int[6];
-		int[] currentFood = initialFood;
+		Fraction[] nextFood = new Fraction[6];
+		Fraction[] currentFood = new Fraction[6];
 		
-		// debug {
 		for (int i=0; i<6; i++) {
-			result += currentFood[i] + " ";
+		    currentFood[i] = new Fraction(new BigInteger(String.valueOf(initialFood[i])));
 		}
-		System.out.println("initial foods=" + result);
-		result = "";
-		// }
+		
+		if (rounds == 0) {
+		    result = String.valueOf(initialFood[SNAUG]);
+		    return result;
+		}
 
 		for (int i=0; i<rounds; i++) {
+		    result = "";
+		    for (int initial=0; initial<6; initial++) {
+		        result += currentFood[initial] + " ";
+		    }
+		    result = "";
+		    
 			for (int dragon=0; dragon<6; dragon++) {
-				nextFood[dragon] = getFood(i, currentFood, dragon);
-				result += nextFood[dragon] + " ";
+			    Fraction next = getFood(i, currentFood, dragon);
+				nextFood[dragon] = next;
+				result += nextFood[dragon].reduce() + " ";
 			}
 			
-			// debug {
-			System.out.println("round " + i + " foods=" + result);
-			result = "";
-			// }
-			
-			for (int dragon=0; dragon<6; dragon++) 
-				currentFood[dragon] = nextFood[dragon]/4;
+			for (int dragon=0; dragon<6; dragon++) {
+				currentFood[dragon] = nextFood[dragon];
+		    }
 		}
 		
-		// debug {
-		result = "";
-		for (int i=0; i<6; i++) {
-			result += nextFood[i] + " ";
-		}
-		result += "/" + (int) Math.pow(4, rounds);
-		// }
-		
-		result = getResult(nextFood[U], rounds);
+		result = nextFood[SNAUG].reduce().toString();
 		
 		return result;
 	}
 	
-	private String getResult(int numerator, int pow) {
-		String result = "";
-		
-		int denominator = 4;	//(int) Math.pow(4, pow);
-		
-		result = reduce(numerator, denominator);
-		System.out.println("getResult(" + numerator + ", " + denominator + ") returns " + result);
-		
-		return result;
-	}
-	
-	private String reduce(int numerator, int denominator) {
-		String result = "";
-		
-		int gcm = findGCM(numerator, denominator);
-
-		if (gcm > 1) {
-			numerator /= gcm;
-			denominator /= gcm;
-		}
-		result = numerator + "/" + denominator;
-		
-		return result;
-	}
-	
-	private int findGCM(int a, int b) {
-		int gcm;
-		
-		if (a>b) gcm = b;
-		else gcm = a;
-		
-		while (a%gcm==0 && b%gcm==0) {
-			gcm--;
-		}
-		
-		System.out.println("findGCM(" + a + "," + b + ") gcm=" + gcm);
-		return gcm;
-	}
-	
-	private int getFood(int round, int[] foods, int side) {
-		int result;
+	private Fraction getFood(int round, Fraction[] foods, int side) {
+	    Fraction result;
 		
 		if (side<2) {
-			result = foods[U] + foods[D] + foods[L] + foods[R];	// need to divide by 4 
+		    result = new Fraction(foods[U]);
+		    result.add(foods[D]);
+		    result.add(foods[L]);
+		    result.add(foods[R]);
 		} else if (side < 4) {
-			result = foods[F] + foods[B] + foods[L] + foods[R]; 
+            result = new Fraction(foods[F]);
+            result.add(foods[B]);
+            result.add(foods[L]);
+            result.add(foods[R]);
 		} else {
-			result = foods[F] + foods[B] + foods[U] + foods[D]; 
+            result = new Fraction(foods[F]);
+            result.add(foods[B]);
+            result.add(foods[U]);
+            result.add(foods[D]);
 		}
+		result.multiply(new Fraction(BigInteger.ONE, new BigInteger(String.valueOf(4))));
 
-		System.out.println("getFood(" + round + "," + side + ") result=" + result);
-		
 		return result;
 	}
 }
--- src/com/noeuli/topcoder/TopCoder.java
+++ src/com/noeuli/topcoder/TopCoder.java
@@ -4,9 +4,10 @@ public class TopCoder {
 	public static void main(String[] args) {
 	    Dragons testInstance = new Dragons();
 		int[] input = { 
-			0, 0, 4, 0, 0, 0
+			1, 2, 3, 4, 5, 6
+//		    0, 0, 4, 0, 0, 0
 		};
-		int input2 = 2;
+		int input2 = 45;
 		String ret = testInstance.snaug(input, input2);
 		System.out.println(ret);
 	}
